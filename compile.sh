0bdfdfdaf80fdda882c8db18ad3f4180951e1216
Daniel Savard <daniels@xsoli.com>
2019-08-22 23:48:31 -0400

    Added Application

--- compile.sh
+++ compile.sh
@@ -2,4 +2,5 @@
 elm make exemple/Sandbox.elm --output exemple/sandbox.js
 elm make exemple/Element.elm --output exemple/element.js
 elm make exemple/Document.elm --output exemple/document.js
+elm make exemple/Application.elm --output exemple/application.js
 
--- src/TimeTravel/Browser.elm
+++ src/TimeTravel/Browser.elm
@@ -2,6 +2,7 @@ module TimeTravel.Browser exposing
   ( sandbox
   , element
   , document
+  , application
   --, program
   -- , programWithOptions
 --  , programWithFlags
@@ -25,7 +26,9 @@ import TimeTravel.Internal.View as View
 import TimeTravel.Internal.Util.Nel as Nel
 
 import Html exposing (Html, div, text)
-import Browser
+import Browser 
+import Browser.Navigation exposing (Key)
+import Url
 
 type Msg msg
   = DebuggerMsg Model.Msg
@@ -40,11 +43,20 @@ type alias OptionsWithFlags flags model msg =
   , subscriptions : model -> Sub msg
   }
 
-type alias OptionsWithDocument flags model msg =
+type alias OptionsForDocument flags model msg =
   { init : flags -> (model, Cmd msg)
   , view : model -> Browser.Document msg
   , update : msg -> model -> (model, Cmd msg)
   , subscriptions : model -> Sub msg
+  }
+
+type alias OptionsForApplication flags model msg =
+  { init : flags -> Url.Url -> Key -> (model, Cmd msg)
+  , view : model -> Browser.Document msg
+  , update : msg -> model -> (model, Cmd msg)
+  , subscriptions : model -> Sub msg
+  , onUrlRequest : Browser.UrlRequest -> msg
+  , onUrlChange : Url.Url -> msg
   }
 
 
@@ -118,6 +130,27 @@ document stuff =
     Browser.document (wrapDocument options stuff)
 
 
+{-| See [Html.programWithFlags](http://package.elm-lang.org/packages/elm-lang/html/latest/Html#programWithFlags)
+-}
+application :
+  { init : flags -> Url.Url -> Key -> (model, Cmd msg)
+  , view : model -> Browser.Document msg
+  , update : msg -> model -> (model, Cmd msg)
+  , subscriptions : model -> Sub msg
+  , onUrlRequest : Browser.UrlRequest -> msg
+  , onUrlChange : Url.Url -> msg
+  }
+  -> Program flags (Model model msg) (Msg msg)
+application stuff =
+  let
+    options = 
+      { outgoingMsg = always Cmd.none
+      , incomingMsg = always Sub.none
+      }
+  in    
+    Browser.application (wrapApplication options stuff)
+
+
 wrap :
   { outgoingMsg : OutgoingMsg -> Cmd Never
   , incomingMsg : (IncomingMsg -> (Msg msg)) -> Sub (Msg msg)
@@ -175,8 +208,8 @@ wrapDocument :
   { outgoingMsg : OutgoingMsg -> Cmd Never
   , incomingMsg : (IncomingMsg -> (Msg msg)) -> Sub (Msg msg)
   }
-  -> OptionsWithDocument flags model msg
-  -> OptionsWithDocument flags (Model model msg) (Msg msg)
+  -> OptionsForDocument flags model msg
+  -> OptionsForDocument flags (Model model msg) (Msg msg)
 wrapDocument { outgoingMsg, incomingMsg } { init, view, update, subscriptions } =
   let
     init_ flags =
@@ -221,4 +254,65 @@ wrapDocument { outgoingMsg, incomingMsg } { init, view, update, subscriptions } 
     , update = update_
     , view = view_
     , subscriptions = subscriptions_
+    }
+
+
+wrapApplication :
+  { outgoingMsg : OutgoingMsg -> Cmd Never
+  , incomingMsg : (IncomingMsg -> (Msg msg)) -> Sub (Msg msg)
+  }
+  -> OptionsForApplication flags model msg
+  -> OptionsForApplication flags (Model model msg) (Msg msg)
+wrapApplication { outgoingMsg, incomingMsg } { init, view, update, subscriptions, onUrlRequest, onUrlChange } =
+  let
+    init_ flags url key =
+      let
+        (model, cmd) = init flags url key
+      in
+        (Model.init model, Cmd.batch [ Cmd.map (\msg -> UserMsg (Just 0, msg)) cmd ])
+
+    update_ msg model =
+      case msg of
+        UserMsg msgWithId ->
+          let
+            (m, c1) =
+              updateOnIncomingUserMsg (\(id, msg_) -> UserMsg (Just id, msg_)) update msgWithId model
+
+            (m_, c2) =
+              Update.updateAfterUserMsg outgoingMsg m
+          in
+            (m_, Cmd.batch [ c1, Cmd.map DebuggerMsg c2 ])
+
+        DebuggerMsg msg_ ->
+          let
+            (m, c) =
+              Update.update outgoingMsg msg_ model
+          in
+            (m, Cmd.batch [ Cmd.map DebuggerMsg c ])
+
+    view_ model =
+      View.document (\c -> UserMsg (Nothing, c)) DebuggerMsg view model
+
+    subscriptions_ model =
+      let
+        item = Nel.head model.history
+      in
+        Sub.batch
+          [ Sub.map (\c -> UserMsg (Nothing, c)) (subscriptions item.model)
+          , incomingMsg (DebuggerMsg << Receive)
+          ]
+
+    onUrlRequest_ url = 
+      UserMsg (Nothing, onUrlRequest url)
+
+    onUrlChange_ url = 
+      UserMsg (Nothing, onUrlChange url)
+
+  in
+    { init = init_
+    , update = update_
+    , view = view_
+    , subscriptions = subscriptions_
+    , onUrlRequest = onUrlRequest_
+    , onUrlChange = onUrlChange_
     }
